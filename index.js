eff04ad1ce06195a3347cfd8c55a957c9015e8f1
Kubaho Linne Heaven <kubaholinne@gmail.com>
2023-03-21 20:15:40 +0530

    defined class

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-let listBooks = [
-  {
-    title: 'Lorem Ipsum',
-    author: 'Testero Testyy',
-  },
-  {
-    title: 'Second Book',
-    author: 'Testero Testyy',
-  },
-];
-
-function addBooks(title, author) {
-  listBooks.push({
-    title,
-    author,
-  });
+class Books {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+  displayBooks() {
+    return `${this.title} by ${this.author}`;
+  }
+  // static fromJSON(json) {
+  //   return new Books(json.title, json.author);
+  // }
+  // listBooks = [
+  //   {
+  //     title: 'Lorem Ipsum',
+  //     author: 'Testero Testyy',
+  //   },
+  //   {
+  //     title: 'Second Book',
+  //     author: 'Testero Testyy',
+  //   },
+  // ];
+  
+  AddBooks(title, author) {
+    listBooks.push({
+      title,
+      author,
+    });
+  }
+  RemoveBooks(index) {
+    listBooks.splice(index, 1);
+  
+    displayBooks(listBooks);
+    localStorage.setItem('listBooks', JSON.stringify(listBooks));
+  }
 }
 
-function displayBooks(listBooks) {
+const Book1 = new Books('Lorem Ipsum','Testero Testyy');
+const Book2 = new Books('Second Book','Testero Testyy');
+
+document.getElementById("demo").innerHTML = myCar1.name + " " + myCar2.name;
+
+function displayBooks(Books) {
   const bookSection = document.querySelector('.fav-books');
   bookSection.innerHTML = listBooks.map((book, index) => `
 <div>
@@ -28,12 +51,7 @@ function displayBooks(listBooks) {
 `).join('');
 }
 
-function removeBooks(index) {
-  listBooks.splice(index, 1);
 
-  displayBooks(listBooks);
-  localStorage.setItem('listBooks', JSON.stringify(listBooks));
-}
 
 function logSubmit(event) {
   const bookTitle = document.querySelector('#book-title').value;
