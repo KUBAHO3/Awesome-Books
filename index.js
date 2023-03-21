20aa010f7bced29bad2d3a8de03ddd634e35fe51
Kubaho Linne Heaven <kubaholinne@gmail.com>
2023-03-21 10:25:32 +0530

    add html& js

---
+++ index.js
@@ -0,0 +1,53 @@
+var listBooks = [
+    {
+        title: "Lorem Ipsum",
+        author: "Testero Testyy",
+    },
+    {
+        title: "Second Book",
+        author: "Testero Testyy",
+    },
+]
+
+function addBooks (title, author) {
+    listBooks.push({
+        title,
+        author,
+    })
+}
+
+displayBooks (listBooks);
+
+function displayBooks (listBooks){
+const bookSection = document.querySelector('.fav-books');
+bookSection.innerHTML = listBooks.map((book, index) => `
+<div>
+<p>${book.title}</p>
+        <p>${book.author}</p>
+        <button type="button" onclick="removeBooks(${index})">remove</button>
+        <hr>
+        </div>
+        `).join('');
+
+}
+
+function removeBooks (index) {
+    listBooks.splice(index, 1)
+
+    displayBooks (listBooks);
+    localStorage.setItem('listBooks', listBooks);
+
+}
+
+function logSubmit(event) {
+    const bookTitle = document.querySelector('#book-title').value;
+    const bookAuthor = document.querySelector('#book-author').value;
+
+    addBooks (bookTitle, bookAuthor)
+    displayBooks (listBooks)
+    localStorage.setItem('listBooks', listBooks);
+
+    console.log(localStorage.getItem('userData'))
+
+    event.preventDefault();
+  }
