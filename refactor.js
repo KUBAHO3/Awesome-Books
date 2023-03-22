25ecb8ce12cfa6dea681cb9dbe8f2b503f8838a9
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-22 13:04:16 +0530

    add add book method

--- refactor.js
+++ refactor.js
@@ -31,6 +31,17 @@ book(title, author) {
     this.form.reset();
   }
 
+  
+addBook(event) {
+    event.preventDefault();
+    const title = event.target[0].value;
+    const author = event.target[1].value;
+    const newBook = this.book(title, author);
+    this.books.push(newBook);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
+
   setupEventListeners() {
     this.form.addEventListener('submit', this.addBook.bind(this));
     this.bookListContainer.addEventListener('click', (e) => {
